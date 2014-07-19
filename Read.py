8ddc994007b9d2320b59e33e9e991cc4e0073aa3
Mario Gomez <mxgxw.alpha@gmail.com>
2014-07-18 21:15:31 -0600

    Read example now loops until Ctrl-C is caught.

--- Read.py
+++ Read.py
@@ -20,6 +20,10 @@ signal.signal(signal.SIGINT, end_read)
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
 
+# Welcome message
+print "Welcome to the MFRC522 data read example"
+print "Press Ctrl-C to stop."
+
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
     
@@ -55,5 +59,3 @@ while continue_reading:
         else:
             print "Authentication error"
 
-        # Make sure to stop scanning for cards
-        continue_reading = False
