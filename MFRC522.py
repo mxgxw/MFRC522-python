68963702c1cc9718c0fc3a6c666cd3e1d65adaa5
Marek2k17 <marek.salzinger@gmail.com>
2018-03-20 10:55:12 +0100

    replace number with variable
    i googled for changing the pin 22, i only found a hint to line 10 so i changed line 113 to use the variable from line 10 instead of the number

--- MFRC522.py
+++ MFRC522.py
@@ -110,7 +110,7 @@ class MFRC522:
   def __init__(self, dev='/dev/spidev0.0', spd=1000000):
     spi.openSPI(device=dev,speed=spd)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(NRSTPD, GPIO.OUT)
     GPIO.output(self.NRSTPD, 1)
     self.MFRC522_Init()
   
