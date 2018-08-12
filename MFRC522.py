d507304690ec2d7e77449582ae5b92287c56adaa
aikasse <aikasse@mac.com>
2018-08-12 20:29:13 +0200

    Minor change: Print "Sector" instead of "Block"
    Dump.py map all data stored in MIFARE Classic 1k card or tag. When Dump.py is executed, prints "Sector" at the beginning of each line of data. I think this is wrong. Each line is a “Block”.
    
    "2.2 Logical Structure
    A MiFare Classic card is in principle a memory card with few extra functionalities. The memory is divided into data blocks of 16 bytes. Those data blocks are grouped into sectors. The MiFare Classic 1k card has 16 sectors of 4 data blocks each..."
    
    Sources:
    http://www.cs.ru.nl/~flaviog/publications/Attack.MIFARE.pdf (p. 4, section 2.2.)
    https://upload.wikimedia.org/wikipedia/commons/3/39/MiFare_Byte_Layout.png

--- MFRC522.py
+++ MFRC522.py
@@ -360,7 +360,7 @@ class MFRC522:
       print "Error while reading!"
     i = 0
     if len(backData) == 16:
-      print "Sector "+str(blockAddr)+" "+str(backData)
+      print "Block "+str(blockAddr)+" "+str(backData)
   
   def MFRC522_Write(self, blockAddr, writeData):
     buff = []
