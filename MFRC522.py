aba738d5358fd8f48f69140c8c6cfa639d459ff6
Noam Nelke <NoamNelke@gmail.com>
2019-04-09 21:58:46 +0300

    support for newer SPI-Py
    this fixes https://github.com/mxgxw/MFRC522-python/issues/69

--- MFRC522.py
+++ MFRC522.py
@@ -128,7 +128,7 @@ class MFRC522:
   serNum = []
   
   def __init__(self, dev='/dev/spidev0.0', spd=1000000):
-    spi.openSPI(device=dev,speed=spd)
+    self.dev_dictionary = spi.openSPI(device=dev,speed=spd)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(self.NRSTPD, GPIO.OUT)
     GPIO.output(self.NRSTPD, 1)
@@ -138,10 +138,10 @@ class MFRC522:
     self.Write_MFRC522(self.CommandReg, self.PCD_RESETPHASE)
   
   def Write_MFRC522(self, addr, val):
-    spi.transfer(((addr<<1)&0x7E,val))
+    spi.transfer(self.dev_dictionary, ((addr<<1)&0x7E,val))
   
   def Read_MFRC522(self, addr):
-    val = spi.transfer((((addr<<1)&0x7E) | 0x80,0))
+    val = spi.transfer(self.dev_dictionary, (((addr<<1)&0x7E) | 0x80,0))
     return val[1]
   
   def SetBitMask(self, reg, mask):
