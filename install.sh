20b08a7f25845b1f86011d5c93798dfaa323b4fc
Lars Repenning <repi@gmx.de>
2022-01-17 01:21:30 +0100

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-sudo apt install pyhton3 -y
+
+sudo apt install python3 -y
 sudo apt install python3-dev gcc -y
 
+cd ~
+
 git clone https://github.com/lthiery/SPI-Py.git
+cd SPI-Py
 sudo python3 setup.py install
 
 sudo apt-get install alsa-utils
